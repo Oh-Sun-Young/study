c627ba5751d80a6d19d007be52dcc053405b7b05
Oh-Sun-Young <115194619+Oh-Sun-Young@users.noreply.github.com>
2024-02-02 20:59:53 +0900

    [인강] 유니티 기초
    [C#과 유니티로 만드는 MMORPG 게임 개발 시리즈]
    Part 3 유니티 엔진 > 섹션 1 유니티 기초

---
+++ Inflearn/RookissMMORPG/3_Unity/MMO_Unity/Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    /* 함수 형태로 호출하면 괄호까지 사용해야 하기 때문에 프로퍼티로 변경
+     * static Managers instance; // 유일성이 보장된다
+     * public static Managers GetInstance() { Init(); return instance; } // 유일한 매니저를 갖고 온다
+     */
+
+    static Managers s_instance; // 유일성이 보장된다
+    public static Managers instance { get { Init(); return s_instance; } } // 유일한 매니저를 갖고 온다
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 초기화
+        /* Manager가 여러개가 있는 경우 덮어쓰는 문제 발생
+         * instance = this;
+         */
+        /* Find를 활용하여 Manager를 지정한 경우 해당 이름을 가진 오브젝트가 없는 경우 Null값이 되어 문제 발생
+         * GameObject go = GameObject.Find("@Managers");
+         * instance = go.GetComponent<Managers>();
+         */
+        Init();
+    }
+
+    static void Init()
+    {
+        // 초기화
+        if (instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if(go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+    }
+}
